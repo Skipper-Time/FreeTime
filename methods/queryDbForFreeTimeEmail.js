5f27f68a9cfb6aca0c8dacc4548aa00ae8ce28d4
Jordan Sweet <hello@jordandsweet.com>
2022-06-30 09:56:28 -0700

    Fix function name

--- methods/queryDbForFreeTimeEmail.js
+++ methods/queryDbForFreeTimeEmail.js
@@ -5,7 +5,7 @@ const db = require('../firebase/firebaseRequire');
 // query USERS database matching EMAIL
 // return access token and refresh token
 //
-module.exports = async function queryDbForTokens(email) {
+module.exports = async function queryDbForFreeTimeEmail(email) {
   const docRef = doc(db, "user_cal_data", email);
   let  docSnap = await getDoc(docRef);
   docSnap = docSnap.data();
