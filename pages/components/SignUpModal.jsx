1ca92303c612c2ad894dee7ec20f5b81241f27d0
Jessica Zhou <jessicazhou541@gmail.com>
2022-06-30 10:22:09 -0700

    more info inserted into db on signup

--- pages/components/SignUpModal.jsx
+++ pages/components/SignUpModal.jsx
@@ -16,8 +16,6 @@ import {
   Link,
 } from '@chakra-ui/react';
 
-
-
 import { useRouter } from 'next/router'
 
 import { setCookies } from 'cookies-next';
@@ -26,7 +24,7 @@ import { GoogleLogin, useGoogleLogin } from '@react-oauth/google';
 import { getAuth, signInWithCredential, GoogleAuthProvider } from "firebase/auth";
 import axios from 'axios';
 import {provider, auth, db} from '../../firebase/firebaseConfig.js';
-import { doc, setDoc, getDoc, collection } from 'firebase/firestore';
+import { doc, setDoc, collection } from 'firebase/firestore';
 
 
 const SignUpModal = ({ isSignupOpen, onSignupClose }) => {
@@ -50,6 +48,24 @@ const SignUpModal = ({ isSignupOpen, onSignupClose }) => {
         const user = await signInWithCredential(auth, credential);
 
         const userRef = doc(db, 'user_cal_data', user.user.email);
+
+        const locations = {
+          'Aaron Bowers': 'Las Vegas, NV',
+          'Andres Arango': 'San Fransisco, CA',
+          'Jessica Zhou': 'Sierra Vista, AZ',
+          'Dustin Deitch': 'Reno, NV',
+          'Jordan Sweet': 'San Diego, CA',
+          'Hang Yin': 'Seattle, WA'
+        }
+
+        const userInstance = collection(db, 'user_cal_data');
+        setDoc(doc(db, 'user_cal_data', user.user.email), {
+          friends: ['arangotang97@gmail.com', 'bowersaaronjames@gmail.com'],
+          displayName: user.user.displayName,
+          profilePic: user.user.photoURL,
+          userId: user.user.uid,
+          location: locations[user.user.displayName] || 'Los Angeles, CA'
+        }, { merge: true });
 
         // TODO check if the account exists
         // this is a problem because on line 41 a token is automatically
@@ -84,7 +100,7 @@ const SignUpModal = ({ isSignupOpen, onSignupClose }) => {
             </Center>
             <Button onClick={() => googleLogin()} mb="0.5rem" w="fit-content" alignSelf="center">
               <Image src="./google-logo.png" alt="" width="24px" mr="0.5rem" />
-              <Text>Sign in with Google</Text>
+              <Text>Sign up with Google</Text>
             </Button>
             <Text fontWeight="300" textAlign="center" fontSize="xs">
               By clicking Sign up, you agree to our{' '}
