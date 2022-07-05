03805a5a1fcbab596e81e08f45a7227af98af78c
Jordan Sweet <hello@jordandsweet.com>
2022-07-05 13:15:25 -0700

    Cleanup unused files

--- methods/queryDbForTokens.js
+++ methods/queryDbForTokens.js
@@ -2,11 +2,8 @@
 import { doc, collection, query, where, getDoc } from "firebase/firestore";
 
 const db = require('../firebase/firebaseRequire');
-// query USERS database matching EMAIL
-// return access token and refresh token
-//
-module.exports = async function queryDbForTokens(email) {
 
+module.exports = async function queryDbForTokens(email) {
   const docRef = doc(db, "user_cal_data", email);
   let  docSnap = await getDoc(docRef);
   docSnap = docSnap.data();
